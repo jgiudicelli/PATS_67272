77a3084dccf1d3688d4d49198a1544483538ea80
profh <profh@cmu.edu>
2012-02-07 22:14:05 -0500

    Ran first set of migrations

--- test/unit/animal_test.rb
+++ test/unit/animal_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class AnimalTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # Not much for testing Animal as it is a simple model
+  # Relationship macros...
+  should have_many(:pets)
+  should have_many(:vaccines)
+  
+  # Validation macros...
+  should validate_presence_of(:name)
 end
