e283175ff7d2c2e52cb0afc31808e9ba302cb81c
profh <profh@cmu.edu>
2012-02-07 21:22:59 -0500

    Base code generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PATS67272::Application.config.secret_token = 'd1bd0204752c0be9af38d786de42af854620f7adaa0ba4f2dcc9fbe3d5989d8267762d87b63137fdf3f0298e5fdf2e8bcdf8b81895481c250a4edf12b2c30ced'
