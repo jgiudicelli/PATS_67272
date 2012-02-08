3cfa48b18a3721e6aa0e9c5dade0c48373dc2c10
profh <profh@cmu.edu>
2012-02-07 22:37:12 -0500

    Forgot to add state to owner the first time

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120208023258) do
+ActiveRecord::Schema.define(:version => 20120208033531) do
 
   create_table "animals", :force => true do |t|
     t.string   "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(:version => 20120208023258) do
     t.boolean  "active"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "state"
   end
 
   create_table "pets", :force => true do |t|
