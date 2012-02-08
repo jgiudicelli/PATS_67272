75e0a865e5ca1187d312894579bd55568b9385b3
profh <profh@cmu.edu>
2012-02-07 21:40:49 -0500

    Added business logic to Owner model

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,2 +1,62 @@
 class Owner < ActiveRecord::Base
+  # create a callback that will strip non-digits before saving to db
+  before_save :reformat_phone
+  
+  # Relationships
+  # -----------------------------
+  has_many :pets # :dependent => :destroy  (:nullify orphans the record)
+  has_many :visits, :through => :pets
+  # belongs_to :user  # implies that there is a user_id field which we will add later
+  
+  
+  # Scopes
+  # -----------------------------
+  # list owners in alphabetical order
+  scope :alphabetical, order('last_name, first_name')
+  # get all the owners who are active (not moved out and pet is alive)
+  scope :active, where('active = ?', true)
+  # search for all the owners in the system by either first or last name
+  scope :search, lambda { |term| where('first_name LIKE ? OR last_name LIKE ?', "#{term}%", "#{term}%").order('last_name, first_name') }
+
+  # Misc Constants
+  # -----------------------------
+  # This is a local vet shop, but it is possible to have people coming from WV and OH as well
+  # Set up a states array to make select menu easier later
+  STATES_LIST = [['Ohio', 'OH'],['Pennsylvania', 'PA'],['West Virginia', 'WV']]
+  # Here is a complete states list if you want to expand the menu options...
+  # STATES_LIST = [['Alabama', 'AL'],['Alaska', 'AK'],['Arizona', 'AZ'],['Arkansas', 'AR'],['California', 'CA'],['Colorado', 'CO'],['Connectict', 'CT'],['Delaware', 'DE'],['District of Columbia ', 'DC'],['Florida', 'FL'],['Georgia', 'GA'],['Hawaii', 'HI'],['Idaho', 'ID'],['Illinois', 'IL'],['Indiana', 'IN'],['Iowa', 'IA'],['Kansas', 'KS'],['Kentucky', 'KY'],['Louisiana', 'LA'],['Maine', 'ME'],['Maryland', 'MD'],['Massachusetts', 'MA'],['Michigan', 'MI'],['Minnesota', 'MN'],['Mississippi', 'MS'],['Missouri', 'MO'],['Montana', 'MT'],['Nebraska', 'NE'],['Nevada', 'NV'],['New Hampshire', 'NH'],['New Jersey', 'NJ'],['New Mexico', 'NM'],['New York', 'NY'],['North Carolina','NC'],['North Dakota', 'ND'],['Ohio', 'OH'],['Oklahoma', 'OK'],['Oregon', 'OR'],['Pennsylvania', 'PA'],['Rhode Island', 'RI'],['South Carolina', 'SC'],['South Dakota', 'SD'],['Tennessee', 'TN'],['Texas', 'TX'],['Utah', 'UT'],['Vermont', 'VT'],['Virginia', 'VA'],['Washington', 'WA'],['West Virginia', 'WV'],['Wisconsin ', 'WI'],['Wyoming', 'WY']]
+  
+  
+  # Validations
+  # -----------------------------
+  # make sure required fields are present
+  validates_presence_of :first_name, :last_name, :email, :phone
+  # if zip included, it must be 5 digits only
+  validates_format_of :zip, :with => /^\d{5}$/, :message => "should be five digits long", :allow_blank => true
+  # phone can have dashes, spaces, dots and parens, but must be 10 digits
+  validates_format_of :phone, :with => /^(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})$/, :message => "should be 10 digits (area code needed) and delimited with dashes only"
+  # email format (other regex for email exist; doesn't allow .museum, .aero, etc.)
+  # Not allowing for .uk, .ca, etc. because this is a Pittsburgh business and customers not likely to be out-of-country
+  validates_format_of :email, :with => /^[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))$/i, :message => "is not a valid format"
+  # if state is given, must be one of the choices given (no hacking this field)
+  validates_inclusion_of :state, :in => %w[PA OH WV], :message => "is not an option", :allow_nil => true, :allow_blank => true
+  # if not limited to the three states, it might be better (but slightly slower) to write:
+  # validates_inclusion_of :state, :in => STATES_LIST.map {|key, value| value}, :message => "is not an option", :allow_nil => true, :allow_blank => true
+   
+  # a method to get owner name in last, first format
+  def name
+    last_name + ", " + first_name
+  end
+  
+  
+  # Callback code
+  # -----------------------------
+   private
+     # We need to strip non-digits before saving to db
+     def reformat_phone
+       phone = self.phone.to_s  # change to string in case input as all numbers 
+       phone.gsub!(/[^0-9]/,"") # strip all non-digits
+       self.phone = phone       # reset self.phone to new string
+     end
+  
 end
