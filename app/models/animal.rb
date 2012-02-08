ef349327c5efa33f866265361fd0a236c4cf4a89
profh <profh@cmu.edu>
2012-02-07 21:35:35 -0500

    Added business logic to Animal model

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,2 +1,15 @@
 class Animal < ActiveRecord::Base
+  # Relationships
+  # -----------------------------
+  has_many :pets
+  has_many :vaccines
+  
+  # Scopes
+  # -----------------------------
+  scope :all, order('name')
+   
+  # Validations
+  # -----------------------------
+  validates_presence_of :name
+  
 end
