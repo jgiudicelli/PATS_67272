05c94319a6636f07de3ea283b9e30b05bda30423
profh <profh@cmu.edu>
2012-02-08 23:01:51 -0500

    Visit views updated

--- app/helpers/visits_helper.rb
+++ app/helpers/visits_helper.rb
@@ -1,2 +1,8 @@
 module VisitsHelper
+  # create a helper to get the options for the pet select menu
+  # will return an array with key being pet_name : owner_name so 
+  # that vet can choose the right pet (many pets share same names)
+  def get_pet_options
+    Pet.all.active.map{|p| ["#{p.name} : #{p.owner.name}", p.id] }
+  end
 end
