4c46aaa74c21881504429e138cba0f3a1298a114
profh <profh@cmu.edu>
2012-02-09 09:22:44 -0500

    Home controller and views added

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,18 @@
+def index
+end
+
+def about
+end
+
+def contact
+end
+
+def privacy
+end
+
+def search
+  @query = params[:query]
+  @owners = Owner.search(@query)
+  @pets = Pet.search(@query)
+  @total_hits = @owners.size + @pets.size
+end
